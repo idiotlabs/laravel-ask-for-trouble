6f8dfa5e10a36a54d1f8bbd9ad53521ccaf6ee69
Seobs <subi88@naver.com>
2019-09-07 16:24:35 +0900

    Add video background sample

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,6 +7,7 @@
 require('./bootstrap');
 
 window.Vue = require('vue');
+window.VideoBg = require('vue-videobg');
 
 /**
  * The following block of code may be used to automatically register your
@@ -19,7 +20,10 @@ window.Vue = require('vue');
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
+Vue.component('video-bg', VideoBg.default);
+
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+Vue.component('landing-component', require('./components/LandingComponent').default);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
