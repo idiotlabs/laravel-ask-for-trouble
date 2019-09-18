0c68d1b00da602011fd3d19ba977764493c68d39
Seobs <subi88@naver.com>
2019-09-18 18:32:35 +0900

    Add contact page

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -16,7 +16,7 @@
         <script src="{{ asset('js/app.js') }}" defer></script>
     @endif
 
-    <!-- Fonts -->
+<!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Noto+Sans+KR" rel="stylesheet">
 
@@ -25,49 +25,25 @@
         <link href="{{ mix('css/app.css') }}" rel="stylesheet">
     @else
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    @endif
+@endif
 
-    <!-- Add icon library -->
+<!-- Add icon library -->
     <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/xeicon@2.3.3/xeicon.min.css">
 </head>
 <body>
 <div id="app">
     <section>
         <video-bg :sources="['{{ asset('assets/video/timelapse.mp4') }}']" img="{{ asset('assets/images/bg.jpg') }}">
-            <h2 class="pt-48 sm:pt-64">누구나 즐기는</h2>
-            <h1 class="text-5xl sm:text-6xl">자유로운 음악 생활</h1>
-            <button class="mt-6 bg-transparent py-4 px-6 border rounded text-lg font-semibold text-white hover:bg-white hover:text-black">
-                <a href="/contact">사서고생 하러 가기</a>
-            </button>
+            <h1 class="pt-48 sm:pt-24 text-5xl sm:text-6xl">010-3066-0209</h1>
+            <span class="text-white">or</span>
+            <div>
+                <img class="w-4/5 xl:w-1/2 inline-block p-4"
+                     src="//t1.daumcdn.net/roughmap/imgmap/dd0dfea0069944d84115c409493a910c96fd620bb69d30c68a48875f77f7444e" >
+            </div>
         </video-bg>
     </section>
 
-    <section>
-        <div class="container mx-auto pt-16">
-            <div class="flex flex-wrap">
-                <div class="w-full md:w-1/3 px-2 mb-6 text-center">
-                    <i class="xi-emoticon-cool-o xi-3x"></i>
-                    <div class="mt-4">최대 10명이서 한 팀으로</div>
-                </div>
-
-                <div class="w-full md:w-1/3 px-2 mb-6 text-center">
-                    <i class="xi-music xi-3x"></i>
-                    <div class="mt-4">커버곡 연주, 자작곡, 영상등 자유롭게</div>
-                </div>
-
-                <div class="w-full md:w-1/3 px-2 mb-6 text-center">
-                    <i class="xi-equalizer-thin xi-3x"></i>
-                    <div class="mt-4">음악을 즐겨보세요</div>
-                </div>
-            </div>
-        </div>
-    </section>
-
-    <section>
-        <landing-grid-component class="pt-10"></landing-grid-component>
-    </section>
-
-    <footer class='bg-gray-900 w-full border-t border-grey mt-10 p-4 text-white'>
+    <footer class='bg-gray-900 w-full border-grey p-4 text-white'>
         <div class="mt-4 font-thin">음악을 좋아하는 분들의 연락을 기다립니다.</div>
         <div class="social-box">
             <i class="xi xi-kakaotalk"></i>
