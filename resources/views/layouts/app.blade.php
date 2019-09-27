871eb453947ab62a715853bc0c557d460cdd7e0a
Seobs <subi88@naver.com>
2019-09-27 18:09:50 +0900

    Add canonical tag

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,16 +3,16 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="title" content="수유리콜라보 사서고생 - 누구나 즐기는 자유로운 음악 생활">
+    <meta name="title" content="누구나 즐기는 자유로운 음악 생활 - 수유리콜라보 사서고생">
     <meta name="keywords" content="음악, 음악생활, 도봉구, 강북구, 기타, 베이스, 건반, 키보드, 드럼, 사서고생">
     <meta name="description" content="자유롭게 모여서 음악을 할 수 있는 곳입니다. 누구든지 음악을 즐기는 마음만 있다면 환영합니다.">
     <meta name="robots" content="index,follow">
     <meta property="og:type" content="website">
-    <meta property="og:title" content="수유리콜라보 사서고생 - 누구나 즐기는 자유로운 음악 생활">
+    <meta property="og:title" content="누구나 즐기는 자유로운 음악 생활 - 수유리콜라보 사서고생">
     <meta property="og:description" content="자유롭게 모여서 음악을 할 수 있는 곳입니다. 누구든지 음악을 즐기는 마음만 있다면 환영합니다.">
     <meta property="og:image" content="">
     <meta property="og:url" content="">
-    <meta itemprop="name" content="수유리콜라보 사서고생 - 누구나 즐기는 자유로운 음악 생활">
+    <meta itemprop="name" content="누구나 즐기는 자유로운 음악 생활 - 수유리콜라보 사서고생">
     <meta itemprop="description" content="자유롭게 모여서 음악을 할 수 있는 곳입니다. 누구든지 음악을 즐기는 마음만 있다면 환영합니다.">
     <meta itemprop="image" content="">
 
@@ -21,7 +21,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>누구나 즐기는 자유로운 음악 생활 - 수유리콜라보 사서고생</title>
 
     <!-- Scripts -->
     @if (app()->environment('local'))
@@ -53,6 +53,8 @@
 
     <!-- Add icon library -->
     <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/xeicon@2.3.3/xeicon.min.css">
+
+    <link rel="canonical" href="{{ url()->current() }}">
 </head>
 <body>
 <div id="app">
