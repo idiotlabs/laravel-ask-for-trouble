9600a8b6cff6e5555b414d08b5b213e691a392e7
Seobs <subi88@naver.com>
2020-05-17 15:18:53 +0900

    Change gallery

--- app/Http/Controllers/LandingController.php
+++ app/Http/Controllers/LandingController.php
@@ -3,12 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
 
 class LandingController extends Controller
 {
     public function index()
     {
-        return view('landing');
+        $photos = File::allFiles(public_path('/assets/images/landing'));
+
+        return view('landing')
+            ->with('photos', $photos);
     }
 
     public function contact()
